94d0ca1a9b3d026ef2e63b3ecac2174e6467bf34
Ruiran <ruiranoliveirasantos@gmail.com>
2021-07-15 16:12:20 -0300

    fixing getdata promise resolve and reject

--- dataset.js
+++ dataset.js
@@ -10,7 +10,7 @@ const publicDataset = {
         MUST_NOT: 3,
     },
 
-    retornaToken: function(){
+    retornaToken: function () {
         let token = {
             'public': keys.token.PUBLIC,
             'secret': keys.token.SECRET,
@@ -18,12 +18,12 @@ const publicDataset = {
         return token;
     },
 
-    retornaOauth: function() {
+    retornaOauth: function () {
         let consumer = {
-            'public': keys.consumer.PUBLIC, 
-            'secret': keys.consumer.SECRET, 
+            'public': keys.consumer.PUBLIC,
+            'secret': keys.consumer.SECRET,
         };
-        
+
         let oauth = new OAuth.OAuth(
             'http://fluig.pormade.com.br:8080/portal/api/rest/oauth/request_token',
             'http://fluig.pormade.com.br:8080/portal/api/rest/oauth/access_token',
@@ -37,18 +37,18 @@ const publicDataset = {
         return oauth;
     },
 
-    createConstraint: function(field, initialValue, finalValue, type, likeSearch) {
+    createConstraint: function (field, initialValue, finalValue, type, likeSearch) {
         return {
             "_field": field,
             "_initialValue": initialValue,
             "_finalValue": finalValue,
             "_type": type || 1,
-            "_likeSearch" : likeSearch || false
+            "_likeSearch": likeSearch || false
         };
     },
 
-    getDataset: function(name, fields, constraints, order) {
-        return new Promise((reject, resolve) => {
+    getDataset: function (name, fields, constraints, order) {
+        return new Promise((resolve, reject) => {
 
             let corpo = JSON.stringify({
                 name,
@@ -56,22 +56,22 @@ const publicDataset = {
                 constraints,
                 order
             });
-           
+
             const token = this.retornaToken();
             const oauth = this.retornaOauth();
-            
+
             oauth.post(
                 'http://fluig.pormade.com.br:8080/api/public/ecm/dataset/datasets',
                 token.public,
                 token.secret,
                 corpo,
                 'application/json',
-                (e, data) => {
-                    if (e) reject(new Error(console.error(e)));
-                    resolve(fs.writeFile('dsk_indica_obras.json', data, (err) => {
-                        if (err) reject(new Error(console.error(err)));
-                        console.log('Salvo!');
-                    }));
+                (e, data, res) => {
+                    if (e) {
+                        reject(e);
+                    } else {
+                        resolve(data);
+                    }
                 }
             );
         });
