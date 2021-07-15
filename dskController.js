846363b99d2034f7fda5556f2ae79c1013b36ad3
Ruiran <ruiranoliveirasantos@gmail.com>
2021-07-15 16:15:11 -0300

    adicionando os tratamentos de erro apropriados para as promises

--- dskController.js
+++ dskController.js
@@ -1,14 +1,34 @@
 const publicDataset = require('./dataset');
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const dskController = {
 
     index: async (req, res) => {
-        fs.unlink('./dsk_indica_obras.json', () => {});
-        await publicDataset.getDataset("dsk_indica_obras", [], [], []).catch((e) => {});
+        let data = '';
+
+        try {
+            console.log('Removendo arquivo...');
+            await fs.unlink('./dsk_indica_obras.json');
+        } catch (error) {
+            console.log('Arquivo não existe.');
+        }
+
+        try {
+            data = String(await publicDataset.getDataset("dsk_indica_obras", [], [], []));
+        } catch (error) {
+            console.log(`Erro ao buscar os dados.`);
+        }
+
+        try {
+            await fs.writeFile('dsk_indica_obras.json', data);
+            console.log('Salvo!');
+        } catch (error) {
+            console.log(`Ocorreu algum erro ao salvar o arquivo: ${error}.`);
+        }
+        
         return res.format({
-            'application/json': () => res.download(path.join(__dirname, 'dsk_indica_obras.json')),
+            'application/json': () => res.download(path.join(__dirname, 'dsk_indica_obras.json'))
         });
     }
 }
